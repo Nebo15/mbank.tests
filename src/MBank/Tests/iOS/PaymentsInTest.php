9a5445a85f6dd7faa869b2ce0e91a4d46df9b92b
napster <evgeniy.furmann@gmail.com>
2015-03-18 16:21:04 +0200

    UI test MBank added

---
+++ src/MBank/Tests/iOS/PaymentsInTest.php
@@ -0,0 +1,78 @@
+<?php
+namespace MBank\Tests\iOS;
+
+class PaymentsInTest extends \MBank\Tests\MBankiOSTestCase
+{
+    public function testPayIn()
+    {
+        $wallet = $this->createWalletAndLoadDashboard();
+        $this->byName('Profile')->click();
+        $this->byName('My cards')->click();
+        $this->waitForElementDisplayedByName('My cards');
+        $this->waitForElementDisplayedByName('Empty list');
+        $this->waitForElementDisplayedByName('Add new card');
+        $this->byName('Add new card')->click();
+        $this->fillCardVisaForm();
+
+        // Back to DashBoard
+        $this->byName('Back to Profile icon')->click();
+        $this->byName('Menu icon')->click();
+
+        $this->walletPayAndCheckBalance();
+
+        // Check Balance in Wallet (API)
+        sleep(1);
+        $wallet_data = $this->getAPIService()->getWallet($wallet->phone, $wallet->password);
+        $this->assertEquals('10010', $wallet_data['data']['amount']);
+    }
+
+    protected function fillCardVisaForm()
+    {
+        // Add card number
+        $this->waitForElementDisplayedByXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIATextField[1]');
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIATextField[1]')->value('4652060724922338');
+        // Add MM
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIATextField[2]')->value('01');
+        // Add YY
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIATextField[3]')->value('17');
+        // Add CVV code
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIATextField[4]')->value('989');
+        // Add CardHolder
+        $this->byName('Done')->click();
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIATextField[5]')->value('testtest');
+        $this->byName('Add card')->click();
+        // Assert Card Is Added
+        $this->waitForElementDisplayedByName('4652 06** **** 2338');
+    }
+
+    public function walletPayAndCheckBalance()
+    {
+        $this->byName('Add funds')->click();
+        $this->waitForElementDisplayedByXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIAStaticText[3]');
+
+        // Check Balance
+        $Balance = $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIAStaticText[3]')->text();
+        $this->assertEquals('10000.00a', $Balance);
+
+        // Pay
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIATextField[1]')->value('10');
+        $this->byName('Done')->click();
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIAButton[1]')->click();
+
+        // Check Transactions
+        $this->waitForElementDisplayedByName('OK');
+        $this->byName('OK')->click();
+        $this->waitForElementDisplayedByXPath('//UIAApplication[1]/UIAWindow[2]/UIATableView[1]/UIATableCell[1]');
+
+        // Back to DashBoard and check the balance
+        $this->byName('Menu icon')->click();
+        $this->waitForElementDisplayedByName('Your balance');
+
+        // Check Balance
+        $Balance2 = $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[3]/UIAStaticText[2]')->text();
+        $this->assertEquals('10010.00a', $Balance2);
+
+    }
+
+}
+
--- src/MBank/Tests/iOS/ProfileTest.php
+++ src/MBank/Tests/iOS/ProfileTest.php
@@ -33,4 +33,59 @@ class ProfileTest extends \MBank\Tests\MBankiOSTestCase
         $wallet_data = $this->getAPIService()->getWallet($wallet->phone, $wallet->password);
         $this->assertTrue(array_key_exists('picture_url', $wallet_data['data']), "Can't find profile image");
     }
+
+    public function testTransferP2P()
+    {
+        $wallet = $this->createWalletAndLoadDashboard();
+        $this->byName('Transfer')->click();
+
+        $this->waitForElementDisplayedByName('Attention!');
+
+        $this->byName('Next')->click();
+
+        // Set Valid Data
+        $this->waitForElementDisplayedByXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[1]');
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[1]')
+            ->value($wallet->person->family_name);
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[2]')
+            ->value($wallet->person->given_name);
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[3]')
+            ->value($wallet->person->patronymic_name);
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[4]')
+            ->value($wallet->person->passport_series_number);
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[5]')
+            ->value($wallet->person->passport_issued_at);
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[6]')
+            ->value($wallet->person->itn);
+
+// TODO Crashed on validation
+//        $this->byName('Next')->click();
+
+        sleep(111);
+    }
+
+    public function testTransferInvalidData()
+    {
+        $wallet = $this->createWalletAndLoadDashboard();
+        $this->byName('Transfer')->click();
+
+        $this->waitForElementDisplayedByName('Attention!');
+
+        $this->byName('Next')->click();
+
+        // Set Invalid Data
+        $this->waitForElementDisplayedByXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[1]');
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[1]')->value('lol');
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[2]')->value('name');
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[3]')->value('test');
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[4]')->value('furman');
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[5]')
+            ->value($wallet->person->passport_issued_at);
+        $this->byXPath('//UIAApplication[1]/UIAWindow[2]/UIAScrollView[4]/UIATextField[6]')->value("44/444");
+
+        $this->byName('Next')->click();
+
+        $this->waitForElementDisplayedByName('Invalid personal number');
+
+    }
 }
