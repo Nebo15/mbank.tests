2964c070d5162084d485170e85e7e8c042a1016e
napster <evgeniy.furmann@gmail.com>
2015-05-19 12:34:24 +0300

    UI Tests Mbank

--- src/MBank/Tests/iOS/TransactionsTest.php
+++ src/MBank/Tests/iOS/TransactionsTest.php
@@ -8,95 +8,106 @@ class TransactionsTest extends \MBank\Tests\MBankiOSTestCase
 
     public function testRepeatPay()
     {
-        $wallet = $this->createWalletAndLoadDashboard();
-        $this->waitForElementDisplayedByElement('Your_balance_Button');
-        $this->byElement('Profile_Button')->click();
-        $this->byElement('Cards_Button')->click();
-        $this->waitForElementDisplayedByElement('Cards_Button');
-        $this->waitForElementDisplayedByElement('Empty_list_Button');
-        $this->waitForElementDisplayedByElement('Add_New_card_Button');
-        $this->waitForElementDisplayedByElement('Back_to_Profile_Button');
-        // Add Card
-        $this->byElement('Add_New_card_Button')->click();
-        $this->fillCardForm('4652060724922338', '01', '17', '989', 'testtest');
-        // Assert Card Is Added
-        $this->waitForElementDisplayedByElement('First_Card_Assert');
-        // Back to DashBoard
-        $this->byElement('Back_to_Profile_Button')->click();
-        $this->byElement('Back_dashboard')->click();
-        $this->cardPayServices();
-        $this->waitForElementDisplayedByElement('Repeat');
-        // Pay from card retry
-        $this->retryPayCard();
+        if (APP_ENV == 'ios') {
+            $wallet = $this->createWalletAndLoadDashboard();
+            $this->waitForElementDisplayedByElement('Your_balance_Button');
+            $this->byElement('Profile_Button')->click();
+            $this->byElement('Cards_Button')->click();
+            $this->waitForElementDisplayedByElement('Cards_Button');
+            $this->waitForElementDisplayedByElement('Empty_list_Button');
+            $this->waitForElementDisplayedByElement('Add_New_card_Button');
+            $this->waitForElementDisplayedByElement('Back_to_Profile_Button');
+            // Add Card
+            $this->byElement('Add_New_card_Button')->click();
+            $this->fillCardForm('4652060724922338', '01', '17', '989', 'testtest');
+            // Assert Card Is Added
+            $this->waitForElementDisplayedByElement('First_Card_Assert');
+            // Back to DashBoard
+            $this->byElement('Back_to_Profile_Button')->click();
+            $this->byElement('Back_dashboard')->click();
+            $this->cardPayServices();
+            $this->waitForElementDisplayedByElement('Repeat');
+            // Pay from card retry
+            $this->retryPayCard();
+        }  elseif (APP_ENV == 'web') {
+            $this->markTestSkipped("Issue not resolved for WEB_APP");
+        }
         // Delete wallet
         if (ENVIRONMENT == 'DEV') {
-            $this->getAPIService()->deleteWallet($wallet->phone);
+            $this->getAPIService()->deleteWallet($this->$wallet->phone);
         }
     }
 
     public function testRepeatPayWithChanges()
     {
-        $wallet = $this->createWalletAndLoadDashboard();
-        $this->waitForElementDisplayedByElement('Your_balance_Button');
-        $this->byElement('Profile_Button')->click();
-        $this->byElement('Cards_Button')->click();
-        $this->waitForElementDisplayedByElement('Cards_Button');
-        $this->waitForElementDisplayedByElement('Empty_list_Button');
-        $this->waitForElementDisplayedByElement('Add_New_card_Button');
-        $this->waitForElementDisplayedByElement('Back_to_Profile_Button');
-        // Add Card
-        $this->byElement('Add_New_card_Button')->click();
-        $this->fillCardForm('4652060724922338', '01', '17', '989', 'testtest');
-        // Assert Card Is Added
-        $this->waitForElementDisplayedByElement('First_Card_Assert');
-        // Back to DashBoard
-        $this->byElement('Back_to_Profile_Button')->click();
-        $this->byElement('Back_dashboard')->click();
-        $this->waitForElementDisplayedByElement('Profile_Button');
-        $this->byElement('Transfer_Button')->click();
-        $this->waitForElementDisplayedByElement('Verification');
-        $this->byElement('Next_Button')->click();
-        // Set Valid Data
-        $this->fillIndentForm($wallet); //TODO заменить методом с APIService.php
-        // Personified User
-        $this->getAPIService()->verifyWallet($wallet->phone);
-        // Check P2P Button
-        $this->byElement('Your_balance_Button')->click();
-        $this->byElement('Profile_Button')->click();
-        $this->byElement('Menu_Button')->click();
-        $this->byElement('Transfer_Button')->click();
-        $this->waitForElementDisplayedByElement('Assert_Element');
-        // Pay into friend wallet
-        $phone_number = $this->byElement('Family_name');
-        $phone_number->click();
-        $phone_number->clear();
-        $phone_number->value('+380931254212');
-        // Fill pay form
-        $this->byElement('Given_name')->value('10');
-        $this->byElement('PayField')->value('BatmanPay');
-        $this->byElement('Done_Button')->click();
-        $this->byElement('Assert_Element')->click();
-        $this->waitForElementDisplayedByElement('Payment_method');
-        sleep(2);
-        // Card Pay
-        $this->byElement('Select_Card')->click();
-        $this->byElement('Pay_button')->click();
-        // Check Transaction in List
-        $this->waitForElementDisplayedByElement('OK_Button');
-        $this->acceptAlert();
-        $this->waitForElementDisplayedByElement('Transactions_Assert');
-        // Repeat Pay With Changes
-        $this->repeatPay();
-        // Assert Transactions in List
-        $this->waitForElementDisplayedByElement('Transactions_Assert');
+        if (APP_ENV == 'ios') {
+            $wallet = $this->createWalletAndLoadDashboard();
+            $this->waitForElementDisplayedByElement('Your_balance_Button');
+            $this->byElement('Profile_Button')->click();
+            $this->byElement('Cards_Button')->click();
+            $this->waitForElementDisplayedByElement('Cards_Button');
+            $this->waitForElementDisplayedByElement('Empty_list_Button');
+            $this->waitForElementDisplayedByElement('Add_New_card_Button');
+            $this->waitForElementDisplayedByElement('Back_to_Profile_Button');
+            // Add Card
+            $this->byElement('Add_New_card_Button')->click();
+            $this->fillCardForm('4652060724922338', '01', '17', '989', 'testtest');
+            // Assert Card Is Added
+            $this->waitForElementDisplayedByElement('First_Card_Assert');
+            // Back to DashBoard
+            $this->byElement('Back_to_Profile_Button')->click();
+            $this->byElement('Back_dashboard')->click();
+            $this->waitForElementDisplayedByElement('Profile_Button');
+            $this->byElement('Transfer_Button')->click();
+            $this->waitForElementDisplayedByElement('Verification');
+            $this->byElement('Next_Button')->click();
+            // Set Valid Data
+            $this->fillIndentForm($wallet); //TODO заменить методом с APIService.php
+            // Personified User
+            $this->getAPIService()->verifyWallet($wallet->phone);
+            // Check P2P Button
+            $this->byElement('Your_balance_Button')->click();
+            $this->byElement('Profile_Button')->click();
+            $this->byElement('Menu_Button')->click();
+            $this->byElement('Transfer_Button')->click();
+            $this->waitForElementDisplayedByElement('Assert_Element');
+            // Pay into friend wallet
+            $phone_number = $this->byElement('Family_name');
+            $phone_number->click();
+            $phone_number->clear();
+            $phone_number->value('+380931254212');
+            // Fill pay form
+            $this->byElement('Given_name')->value('10');
+            $this->byElement('PayField')->value('BatmanPay');
+            $this->byElement('Done_Button')->click();
+            $this->byElement('Assert_Element')->click();
+            $this->waitForElementDisplayedByElement('Payment_method');
+            sleep(2);
+            // Card Pay
+            $this->byElement('Select_Card')->click();
+            $this->byElement('Pay_button')->click();
+            // Check Transaction in List
+            $this->waitForElementDisplayedByElement('OK_Button');
+            $this->acceptAlert();
+            $this->waitForElementDisplayedByElement('Transactions_Assert');
+            // Repeat Pay With Changes
+            $this->repeatPay();
+            // Assert Transactions in List
+            $this->waitForElementDisplayedByElement('Transactions_Assert');
+        }  elseif (APP_ENV == 'web') {
+            $this->markTestSkipped("Issue not resolved for WEB_APP");
+        }
         // Delete wallet
         if (ENVIRONMENT == 'DEV') {
-            $this->getAPIService()->deleteWallet($wallet->phone);
+            $this->getAPIService()->deleteWallet($this->$wallet->phone);
         }
     }
 
     public function testPayOutMultibankWallet()
     {
+        if (APP_ENV == 'web') {
+            $this->markTestSkipped("Issue not resolved for WEB_APP");
+        }
         $wallet = $this->createWalletAndLoadDashboard();
         $this->waitForElementDisplayedByElement('Your_balance_Button');
         $this->walletPayServiceMultibank($wallet);
@@ -110,7 +121,7 @@ class TransactionsTest extends \MBank\Tests\MBankiOSTestCase
         // Check Balance in Wallet (API)
         sleep(1);
         $wallet_data = $this->getAPIService()->getWallet($wallet->phone, $wallet->password);
-        $this->assertEquals($Balance, $wallet_data['data']['amount'].'.00a');
+        $this->assertEquals($Balance, $wallet_data['data']['amount'] . '.00a');
         // Delete wallet
         if (ENVIRONMENT == 'DEV') {
             $this->getAPIService()->deleteWallet($wallet->phone);
