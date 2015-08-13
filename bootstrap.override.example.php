345270cb950bf2659dcd8a6d50557f624369c369
Andrew Dryga <andrew@dryga.com>
2015-08-13 19:59:46 +0300

    Return damaged sample file

--- bootstrap.override.example.php
+++ bootstrap.override.example.php
@@ -1,19 +1,20 @@
 <?php
-//$config = [
-//    'environments' => [
-//        'production' => [
-//            'ios' => '',
-//            'web' => ''
-//        ],
-//        'staging' => [
-//            'ios' => '',
-//            'web' => ''
-//        ],
-//        'development' => [
-//            'ios' => '/Users/andrew/Downloads/build/MBank/derived/Build/Products/Debug-iphonesimulator/MBank.app',
-//            'web' => ''
-//        ]
-//    ],
-//    'environment' => 'development',
-//    'platform' => 'ios',
-//];
+/* This is a sample file, you need to copy it to bootstrap.override.php to set-up your own environment. */
+$config = [
+    'environments' => [
+        'production' => [
+            'ios' => '',
+            'web' => ''
+        ],
+        'staging' => [
+            'ios' => '',
+            'web' => ''
+        ],
+        'development' => [
+            'ios' => '/Users/andrew/Downloads/build/MBank/derived/Build/Products/Debug-iphonesimulator/MBank.app',
+            'web' => ''
+        ]
+    ],
+    'environment' => 'development',
+    'platform' => 'ios',
+];
