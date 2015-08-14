959038429cd267ed1bbb52cc27ea47dbeba96e48
napster <evgeniy.furmann@gmail.com>
2015-08-14 17:03:34 +0300

    update

--- bootstrap.override.example.php
+++ bootstrap.override.example.php
@@ -1,20 +1,20 @@
 <?php
 /* This is a sample file, you need to copy it to bootstrap.override.php to set-up your own environment. */
-$config = [
-    'environments' => [
-        'production' => [
-            'ios' => '',
-            'web' => ''
-        ],
-        'staging' => [
-            'ios' => '',
-            'web' => ''
-        ],
-        'development' => [
-            'ios' => '/Users/andrew/Downloads/build/MBank/derived/Build/Products/Debug-iphonesimulator/MBank.app',
-            'web' => ''
-        ]
-    ],
-    'environment' => 'development',
-    'platform' => 'ios',
-];
+//$config = [
+//    'environments' => [
+//        'PROD' => [
+//            'ios' => '',
+//            'web' => ''
+//        ],
+//        'STG' => [
+//            'ios' => '',
+//            'web' => ''
+//        ],
+//        'DEV' => [
+//            'ios' => '/Users/andrew/Downloads/build/MBank/derived/Build/Products/Debug-iphonesimulator/MBank.app',
+//            'web' => ''
+//        ]
+//    ],
+//    'platform' => 'ios',
+//    'environment' => 'DEV',
+//];
